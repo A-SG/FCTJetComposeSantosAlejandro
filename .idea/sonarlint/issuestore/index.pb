6fa3943363fea125e90c081f0dae4d0564ef663c
Alejandro Santos Gutierrez <alejandrosantosgut@gmail.com>
2023-05-18 08:56:11 +0200

    Navegación entre MainActivity.kt y SecondActivity mediante intent

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+y
+Iapp/src/main/java/com/example/jetcomposesantosalejandro/ui/theme/Shape.kt,7\0\704cb469e0cab3d42da2e614c4e883533d8d6d9a
+x
+Happ/src/main/java/com/example/jetcomposesantosalejandro/ui/theme/Type.kt,6\1\618ec1ad8335308391cc7d16edeb1755acdaa165
+y
+Iapp/src/main/java/com/example/jetcomposesantosalejandro/ui/theme/Theme.kt,5\3\53ee92e9f60fb8499f9b65d1357c75e84fb8abc5
+†
+Vapp/src/main/java/com/example/jetcomposesantosalejandro/data/model/FacturasProvider.kt,d\f\df88a8a268f33d951be0ddd67deeef86fee6c5df
+y
+Iapp/src/main/java/com/example/jetcomposesantosalejandro/ui/theme/Color.kt,d\c\dcb22d7fcc040f75818bb1cf5f21653d9f83f2b4
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
--- app/src/main/java/com/example/jetcomposesantosalejandro/MainActivity.kt
+++ app/src/main/java/com/example/jetcomposesantosalejandro/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.jetcomposesantosalejandro
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toolbar
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -19,13 +24,18 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 import com.example.jetcomposesantosalejandro.data.network.domain.model.Factura
 import dagger.hilt.android.AndroidEntryPoint
+import hilt_aggregated_deps._com_example_jetcomposesantosalejandro_FacturasViewModel_HiltModules_KeyModule
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     private val facturasViewModel by viewModels<FacturasViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -34,62 +44,61 @@ class MainActivity : ComponentActivity() {
                 ListaFactura(listaFacturas = facturasViewModel.listaFacturaResponse)
                 facturasViewModel.getListaFacturas()
             }
-
-
         }
     }
-}
-/*
-@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
-@Composable
-fun ContenedorVista() {
-    Scaffold(topBar = { Toolbar() })
-}*/
+    @Composable
+    fun Toolbar() {
+        TopAppBar(
+            modifier = Modifier
+                .fillMaxWidth()
+                .size(120.dp),
+            title = {
+                Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
+                    Column() {
+                        Button(
+                            onClick = { /*....*/ },
+                            colors = ButtonDefaults.buttonColors(
+                                backgroundColor = Color(0xFFFFFFFF),
+                                contentColor = Color(0xFF79BC2C))
+                        ) {
+                            Text(
+                                text = stringResource(id = R.string.activityMain_toolbar_tvConsumo),
+                                color = colorResource(id = R.color.green)
+                            )
+                        }
 
-@Preview
-@Composable
-fun Toolbar() {
-    TopAppBar(
-        modifier = Modifier
-            .fillMaxWidth()
-            .size(120.dp),
-        title = {
-            Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
-                Column() {
-                    Button(
-                        onClick = { /*....*/ },
-                        colors = ButtonDefaults.buttonColors(
-                            backgroundColor = Color(0xFFFFFFFF),
-                            contentColor = Color(0xFF79BC2C))
-                    ) {
                         Text(
-                            text = stringResource(id = R.string.activityMain_toolbar_tvConsumo),
-                            color = colorResource(id = R.color.green),
+                            text = stringResource(id = R.string.activityMain_toolbar_tvFacturas),
+                            color = colorResource(id = R.color.black),
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 30.sp
+
                         )
                     }
-
-                    Text(
-                        text = stringResource(id = R.string.activityMain_toolbar_tvFacturas),
-                        color = colorResource(id = R.color.black),
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 30.sp
-
-                    )
+                    //ImageButton()
+                    Image(painter = painterResource(id = R.drawable.icono_filtro), contentDescription = null, modifier = Modifier.size(48.dp, 48.dp)
+                        .clickable { val navigate = Intent(this@MainActivity,SecondActivity::class.java)
+                        startActivity(navigate)})
                 }
-                Image(painter = painterResource(id = R.drawable.icono_filtro), contentDescription = null, modifier = Modifier
-                    .size(48.dp, 48.dp))
-            }
 
-        },
-        backgroundColor = colorResource(id = R.color.white),
-    )
-}
-
-
-
-@Composable
-fun ListaFactura(listaFacturas: List<Factura>){
-    LazyColumn(){
-        itemsIndexed(items = listaFacturas){index, item ->  ItemView(factura = item)}
+            },
+            backgroundColor = colorResource(id = R.color.white),
+        )
     }
+
+
+
+    @Composable
+    fun ListaFactura(listaFacturas: List<Factura>) {
+        LazyColumn(){
+            itemsIndexed(items = listaFacturas){index, item ->  ItemView(factura = item)}
+        }
+    }
+
+
 }
+
+
+
+
+
